04e8ddbde0d643427c6a22d0283c39e05fa94073
nayami <nayamior@163.com>
2023-12-19 16:58:47 +0000

    update filelist and message list for A client

--- src/components/AClient.vue
+++ src/components/AClient.vue
@@ -8,6 +8,18 @@ const link = ref(target.link)
 watch(link, (newVal) => {
     target.setLink(newVal)
 })
+
+function goFileList() {
+    let link = 'http://' + target.link + '/filesReceived'
+    // 打开一个链接在后台页面
+    window.open(link, '_blank')
+}
+function goMessage() {
+    let messageLink = 'http://' + target.link + '/messagesreceived'
+    // 打开一个链接在后台页面
+    window.open(messageLink, '_blank')
+}
+
 </script>
 <script>
 export const useTarget = defineStore('Atarget', () => {
@@ -38,9 +50,25 @@ export const useTarget = defineStore('Atarget', () => {
                 <RouterLink to="/mountmessage">挂载消息</RouterLink>
             </nav>
         </div>
-
+        <div style="display: flex; flex-direction: row; justify-content: center; align-items: center;">
+            <button type="button" @click="goFileList" class="fileList">File list</button>
+            <span style="width: 20px;"></span>
+            <button type="button" @click="goMessage" class="fileList">Message</button>
+        </div>
 
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.fileList {
+    width: 100px;
+    height: 30px;
+    border-radius: 10px;
+    background-color: #00a1d6;
+    color: white;
+    font-size: 20px;
+    border: none;
+    outline: none;
+    cursor: pointer;
+}
+</style>
